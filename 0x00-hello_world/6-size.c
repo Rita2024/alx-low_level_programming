1e914159d392559a95f315e9a36477617825919d
Rita2024 <ritaejezie2@gmail.com>
2023-03-16 15:33:21 -0700

    data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int D;
 	float E;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)Sizeof(A));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)Sizeof(B));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)Sizeof(C));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)Sizeof(D));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)Sizeof(E));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(E));
 return (0);
 }
