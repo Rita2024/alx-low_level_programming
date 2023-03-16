1ac33c62fc92e5b90d863321cf3f8f3593da6ac7
Rita2024 <ritaejezie2@gmail.com>
2023-03-16 15:14:48 -0700

    print the sizes of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,16 @@
 
 int main(void)
 {
-	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("The size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
-	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
-	return (0);
+	char A;
+	int B;
+	long int C;
+	long long int D;
+	float E;
+
+printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(A));
+printf("The size of a int: %lu byte(s)\n", (unsigned long)sizeof(B));
+printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(C));
+printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(D));
+printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(E));
+return (0);
 }
