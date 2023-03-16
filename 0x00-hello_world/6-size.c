cb513b5ba6f7ba3a823c5074d395e196d7d26045
Rita2024 <ritaejezie2@gmail.com>
2023-03-16 15:43:17 -0700

    Size of chart

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - print sizes of data type in c
- *
  * Return: 0 successful
  */
-
 int main(void)
 {
 	char A;
@@ -13,7 +10,6 @@ int main(void)
 	long int C;
 	long long int D;
 	float E;
-
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(A));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(B));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(C));
