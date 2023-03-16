33b07fe71315fe31270d1438793e7cdef00d1b2c
Rita2024 <ritaejezie2@gmail.com>
2023-03-16 15:04:22 -0700

    size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+
 /**
- * main -print sizes of data types in c
+ * main - print sizes of data type in c
  *
- * Return: 0 is successful
+ * Return: 0 successful
  */
-int main(void) 
+
+int main(void)
 {
-    printf("The size of int is %zu bytes\n", sizeof(int));
-    printf("The size of char is %zu bytes\n", sizeof(char));
-    printf("The size of float is %zu bytes\n", sizeof(float));
-    printf("The size of double is %zu bytes\n", sizeof(double));
-    printf("The size of long is %zu bytes\n", sizeof(long));
-    printf("The size of long long is %zu bytes\n", sizeof(long long));
-    printf("The size of short is %zu bytes\n", sizeof(short));
-    printf("The size of unsigned int is %zu bytes\n", sizeof(unsigned int));
-    printf("The size of unsigned char is %zu bytes\n", sizeof(unsigned char));
-    printf("The size of unsigned long is %zu bytes\n", sizeof(unsigned long));
-    printf("The size of unsigned long long is %zu bytes\n", sizeof(unsigned long long));
-    printf("The size of unsigned short is %zu bytes\n", sizeof(unsigned short));
-    return (0);
+	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("The size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
 }
