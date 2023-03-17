40ee189d90f63ba3fa54df2bd8d20fca84dc3848
Rita2024 <ritaejezie2@gmail.com>
2023-03-17 16:36:53 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - peint alphabet in lower case
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alc;
+	for alc = 'a'; alc <= 'z'; alc++
+	{
+		putchar(alc);
+	}
+	putchar('\n');
+	return (0);
+}
