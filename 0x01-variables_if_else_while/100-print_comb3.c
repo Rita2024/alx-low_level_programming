e6d74468c8cc1ea220f141dd8b14f62bd201e5e9
Rita2024 <ritaejezie2@gmail.com>
2023-03-17 20:31:04 -0700

    different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,14 +12,17 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (r = 1; r < 10 ; r++)
+		for (r = 1 ; r < 10 ; r++)
 		{
 			if (i < r && i != r)
 			{
 				putchar(i + '0');
-					putchar(r + '0');
+				putchar(r + '0');
+				if (i + r != 17)
+				{
 					putchar(',');
 					putchar(' ');
+				}
 			}
 		}
 	}
