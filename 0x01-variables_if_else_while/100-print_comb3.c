6b27fd7e0b867787ac7317844438a5f118aa28bf
Rita2024 <ritaejezie2@gmail.com>
2023-03-17 19:45:49 -0700

    different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int r;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (r = 1; r < 10 ; r++)
+		{
+			if (i < r && i != r)
+			{
+				putchar(i + '0');
+					putchar(r + '0');
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
