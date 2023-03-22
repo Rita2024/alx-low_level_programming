b97028e39ddaf2c151b17ccd68bec55292c3e8cd
Rita2024 <ritaejezie2@gmail.com>
2023-03-22 12:12:10 -0700

    first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,39 +6,16 @@
 
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
-
-	for (count = 0 ; count < 92 ; count++)
-	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-		fib1 = fib2;
-		fib2 = sum;
-	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (count = 93 ; count < 99 ; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000;
-		}
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
-	}
-	printf("\n");
-	return (0);
+	a = 1
+	b = 2
+	count = 2
+	print(a, end=", ")
+	print(b, end=", ")
+	while count < 98:
+    	c = a + b
+    	print(c, end=", ")
+    	a = b
+    	b = c
+    	count += 1
+	print(a + b)
 }
