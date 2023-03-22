439b59d5c34fb4af06445348ce7fdbadb7c8a93e
Rita2024 <ritaejezie2@gmail.com>
2023-03-22 12:26:26 -0700

    first 50 fibonacci numberz

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-/**
- * main - prints the first 52 fibonacci numbers
- * Return: 0 always
- */
 
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int p = 0;
-	long j = 1, k = 2;
+	int c;
+	long int n1, n2, fn;
 
-	while (p < 50)
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
 	{
-		if (p == 0)
-			printf(", %ld", j);
-		else if (p == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-		++j;
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
 	}
 	printf("\n");
 	return (0);
