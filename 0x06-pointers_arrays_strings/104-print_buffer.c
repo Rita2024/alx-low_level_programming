d8810728d9d8a5f17656774d233ae670df0be558
Rita2024 <ritaejezie2@gmail.com>
2023-03-30 19:24:01 -0700

    magic

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,12 +21,12 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - 0 < 10 ? size - 0 : 10;
-		printf("%08x:", 0);
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x:", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
-				printf("%02x:", (b + o + i));
+				printf("%02s", (b + o + i));
 			else
 				printf(" ");
 			if (i % 2)
