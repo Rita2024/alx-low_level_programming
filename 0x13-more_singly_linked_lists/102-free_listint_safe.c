21304f2fee06d92ebf8020b07aee0ae72f86df18
Rita2024 <ritaejezie2@gmail.com>
2023-05-02 11:57:26 -0700

    free safe version

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *tmp;
+	listp_t *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((tmp = current) != NULL)
+		{
+			current = current->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: head of the listint_t list
+ * Return: the size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *current;
+
+	hptr = NULL;
+	while (*h != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (i);
+			}
+		}
+
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		i++;
+	}
+
+	*h = NULL;
+	free_listp2(&hptr);
+	return (i);
+}
