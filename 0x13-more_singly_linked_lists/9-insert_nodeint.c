fd1a2e5867d0fdeac9a335216cd4bb442ae163a8
Rita2024 <ritaejezie2@gmail.com>
2023-05-02 05:51:31 -0700

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (x = 0; x < idx && tmp != NULL; ++x)
+		for (x = 0; x < idx - 1 && tmp != NULL; ++x)
 		{
 			tmp = tmp->next;
 		}
